ca60a8bdf9eb5edaa16c5af6bb738750a13a7eb5
JasonLimJS <jason94lim@gmail.com>
2020-11-10 02:16:18 +0800

    FRMViz flask framework

--- Web App/index.py
+++ Web App/index.py
@@ -86,5 +86,58 @@ def compute():
     min_date_str= min(calc_df.Date.tolist()).strftime('%d %b %Y')
     max_date_str= max(calc_df.Date.tolist()).strftime('%d %b %Y')
     var_comp= pd.DataFrame(calc_df.groupby('Date')['effective_return'].sum())
+    var_comp.reset_index(inplace=True)
+    var_comp.sort_values(by='effective_return',ascending=True,inplace=True)
+    var_comp_len= len(var_comp)
+    return_List= var_comp.effective_return.tolist()
+    var_pct= 0.05
+    var_lower_index= math.floor(var_pct*var_comp_len)
+    var_upper_index= var_lower_index + 1
+    var_lower= var_comp['effective_return'].iloc[var_lower_index-1]
+    var_upper = var_comp['effective_return'].iloc[var_upper_index - 1]
+    var_final= var_lower + (var_pct*var_comp_len - var_lower_index)*(var_upper- var_lower)
+    var_final_str= str(round(var_final*100,2)) + '%'
+    #print(var_final_str)
+    output_sel_df= sel_df[:]
+    output_sel_df.drop(['position','units'],axis=1,inplace=True)
+    output_sel_df.rename(columns={'ticker':'Ticker','effective_position':'Proportion'},inplace=True)
+    output_sel_df['Unit']= sel_df.units.tolist()
+    output_sel_df['Unit']= output_sel_df.Unit.map(lambda x: int(x*sum_units))
+    output_sel_df['Position']= sel_df.position.tolist()
+    output_sel_df['Position']= output_sel_df.Position.map(lambda x:'Long' if x==1 else 'Short')
+    output_sel_df['Proportion_Desc']= output_sel_df['Proportion'].map(lambda x: str(round(x*100,2)) + '%')
+    tickerList= output_sel_df.Ticker.tolist()
+    propList= output_sel_df.Proportion_Desc.tolist()
+    unitList= output_sel_df.Unit.tolist()
+    positionList= output_sel_df.Position.tolist()
+
+    var_comp_display= calc_df[['Date','variable','return']][:]
+    #print(var_comp_display)
+    var_comp_display_pivot= var_comp_display.pivot(index='Date',columns='variable',values=['return'])
+    col_name= var_comp_display_pivot.columns.tolist()
+    col_name_new= [x[1] + '_' + x[0] for x in col_name]
+    var_comp_display_pivot.columns= col_name_new
+    var_comp_display_pivot.reset_index(inplace=True)
+    var_comp_display_pivot_2= var_comp_display_pivot.merge(var_comp,left_on='Date',right_on='Date',how='left')
+    var_comp_display_pivot_2.rename(columns={'effective_return':'Portfolio Return'},inplace=True)
+    var_comp_display_pivot_2.sort_values(by='Portfolio Return',inplace=True)
+    var_comp_display_pivot_2.reset_index(inplace=True)
+    var_comp_display_pivot_2.drop('index',inplace=True,axis=1)
+    var_comp_display_pivot_2['Rank']= var_comp_display_pivot_2.index + 1
+    output_list= list()
+    for c in range(len(var_comp_display_pivot_2.columns)):
+        output_list.append(var_comp_display_pivot_2.iloc[:,c].tolist())
+    returnTitleList= var_comp_display_pivot_2.columns.tolist()
+    returnTitleList= np.array(returnTitleList).reshape(-1,1).tolist()
+
+    return {'tickerDictQuery':jsonObj,'returnQuery':return_List,'varTitle':var_final_str,
+            'startDate':min_date_str, 'endDate':max_date_str, 'tickerList': tickerList,
+            'propList': propList, 'unitList': unitList, 'positionList': positionList,
+            'returnTitleList': returnTitleList, 'returnOutput': output_list}
+
+if __name__=="__main__":
+    db.create_all()
+    app.run(debug=True)
+
 
 

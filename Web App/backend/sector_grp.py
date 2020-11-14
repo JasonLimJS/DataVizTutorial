e5571accfebc7486efae574d8165829bf6aff1e7
JasonLimJS <jason94lim@gmail.com>
2020-11-15 00:03:20 +0800

    Aggregate ticker- company by sector.

--- Web App/backend/sector_grp.py
+++ Web App/backend/sector_grp.py
@@ -37,5 +37,41 @@ filtered_data_1['Ticker_Sector']= filtered_data_1.groupby('Sector')['Ticker_Comp
 
 filtered_data_2= filtered_data_1[['Sector','Ticker_Sector']].drop_duplicates()
 filtered_data_2['total']= filtered_data_2.Ticker_Sector.map(lambda x: len(x.split(',')))
+sector_map={'Industrials':'Industrials',
+            'Technology':'Technology',
+            'Financial':'Financial Services',
+            'Consumer Cyclical':'Consumer Cyclical',
+            'Basic Materials':'Basic Materials',
+            'Energy':'Energy',
+            'Healthcare':'Healthcare',
+            'Communication Services':'Communication',
+            'Real Estate':'Real Estate',
+            'Utilities':'Utilities',
+            'Consumer Defensive':'Consumer Defensive'}
+filtered_data_2['Sector']= filtered_data_2.Sector.map(sector_map)
+filtered_data_2.rename(columns={'Ticker_Sector':'Ticker'},inplace=True)
+
+def arrangeSeq(listStr):
+    listStr_list= listStr.split(',')
+    listStr_arr= sorted(list(set(listStr_list)))
+    listStr_out= ','.join(listStr_arr)
+    return listStr_out
+
+filtered_data_2['Ticker']= filtered_data_2.Ticker.map(lambda x: arrangeSeq(x))
+    
 filtered_data_2.to_excel('sector_grp.xlsx',index=False)
 filtered_data_2.to_csv('sector_grp.csv',index=False)
+
+sector_grp= pd.read_csv('sector_grp.csv')
+
+master_list= []
+for listing in sector_grp.Ticker.tolist():
+    master_list.extend(listing.split(','))
+master_list_unique= sorted(list(set(master_list))) 
+with open('all_ticks.pkl','wb') as w:
+    pickle.dump(master_list_unique,w)
+    
+    
+    
+    
+
